3c883b4087730365e595f688699b45c017c3f8a5
tarados2 <jmmros@gmail.com>
2021-02-04 18:06:08 +0100

    primercommit

---
+++ ejerciciopython.1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Script que pregunta el nombre del usuario en la consola y después de que el usuario lo introduzca muestre 
+ por pantalla la cadena ¡Hola <nombre>!, donde <nombre> es el nombre que el usuario haya introducido.
+
+"""
+
+name = input("Introduce tu nombre: ")
+print("¡Hola " + name + “!")
+
